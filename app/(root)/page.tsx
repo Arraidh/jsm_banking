1ec126de3f144e5a6add4056f58ed957d2ef1502
ari.darmika <ari.darmika@gmail.com>
2025-03-04 08:11:11 +0800

    Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/headerBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Kadek" };
+  const loggedIn = {
+    firstName: "Kadek",
+    lastName: "JSM",
+    email: "contact@jsmastery.pro",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={1250.0}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, {}]}
+      />
     </section>
   );
 };
