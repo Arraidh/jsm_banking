b5486f96c0215a44c64dc92d2e26b70bfd8ca39c
ari.darmika <ari.darmika@gmail.com>
2025-03-19 23:46:50 +0800

    feat (auth): create initial page and form component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section className="flex-center size-full max-small px-4">
+      <AuthForm type={"sign-in"} />
+    </section>
+  );
 };
 
 export default SignIn;
